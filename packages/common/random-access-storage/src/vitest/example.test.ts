4d53036640c7321aab9b459e609c60087d3cf385
Dmytro Maretskyi <maretskii@gmail.com>
2024-01-23 16:17:46 +0100

    WIP

--- packages/common/random-access-storage/src/vitest/example.test.ts
+++ packages/common/random-access-storage/src/vitest/example.test.ts
@@ -5,14 +5,14 @@ test('global keys', () => {
   // console.log(Object.keys(globalThis));
 });
 
-test('nodefs', () => {
+test.skip('nodefs', () => {
   const storage = createStorage({ type: StorageType.NODE, root: 'test' });
 
   const file = storage.createDirectory('test').getOrCreateFile('test.txt');
   file.write(0, Buffer.from('hello world'));
 });
 
-test.skip('webfs', async () => {
+test('webfs', async () => {
   const storage = createStorage({ type: StorageType.WEBFS, root: '/tmp/vitest' + Math.random().toString() });
 
   const file = storage.createDirectory('test').getOrCreateFile('test.txt');
--- packages/common/random-access-storage/vitest.config.ts
+++ packages/common/random-access-storage/vitest.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   test: {
     include: ['src/vitest/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    browser: {
+      headless: false,
+    },
   },
   resolve: {
     alias: {
