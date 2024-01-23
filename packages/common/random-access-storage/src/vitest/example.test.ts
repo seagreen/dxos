1e4f9d1de5a0973bd930f1c04e1d1095779691e6
Dmytro Maretskyi <maretskii@gmail.com>
2024-01-23 14:56:09 +0100

    Seems to run in node

--- packages/common/random-access-storage/src/vitest/example.test.ts
+++ packages/common/random-access-storage/src/vitest/example.test.ts
@@ -1,12 +1,19 @@
 import { test } from 'vitest';
-import { StorageType, createStorage } from '../browser';
+import { StorageType, createStorage } from '..';
 
 test('global keys', () => {
-  console.log(Object.keys(globalThis));
+  // console.log(Object.keys(globalThis));
 });
 
-test('webfs', async () => {
-  const storage = createStorage({ type: StorageType.WEBFS, root: 'test' });
+test('nodefs', () => {
+  const storage = createStorage({ type: StorageType.NODE, root: 'test' });
+
+  const file = storage.createDirectory('test').getOrCreateFile('test.txt');
+  file.write(0, Buffer.from('hello world'));
+});
+
+test.skip('webfs', async () => {
+  const storage = createStorage({ type: StorageType.WEBFS, root: '/tmp/vitest' + Math.random().toString() });
 
   const file = storage.createDirectory('test').getOrCreateFile('test.txt');
   await file.write(0, Buffer.from('hello world'));
